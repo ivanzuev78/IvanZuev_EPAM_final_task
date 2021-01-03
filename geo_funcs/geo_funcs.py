377c7355798bc56f516f8237d34c2f0ec1c39f7d
Ivan <ivan78.ru@yandex.ru>
2021-01-03 12:26:00 +0300

    Some changes added

--- geo_funcs/geo_funcs.py
+++ geo_funcs/geo_funcs.py
@@ -1,4 +1,6 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple
+
+from geopy import Nominatim
 
 
 def get_city_center(
@@ -26,3 +28,30 @@ def get_city_center(
                 max_longitude = hotel[longitude_col]
 
     return (min_latitude + max_latitude) / 2, (min_longitude + max_longitude) / 2
+
+
+def get_address_by_position(position: Tuple[float, float]) -> Optional[str]:
+    location = Nominatim(user_agent="ivan78").reverse(f"{position[0]}, {position[1]}")
+    if location is not None:
+        return location.address.replace(",", ";")
+    return None
+
+
+def add_address_to_hotels(hotels: List[List]):
+    for hotel in hotels:
+        hotel.append(get_address_by_position((hotel[4], hotel[5])))
+    return hotels
+
+
+# hotels = [
+#     ["1", "min_latitude", "US", "City", 0, 12.2345],
+#     ["2", "max_latitude", "US", "City", 90, 42],
+#     ["3", "min_longitude", "US", "City", 23, 0],
+#     ["4", "max_longitude", "US", "City", 55, 90],
+#     ["5", "Name", "US", "City", 78, 74.999],
+#     ["6", "Name", "US", "City", 33, 59.001],
+#     ["7", "Name", "US", "City", 89, 12],
+# ]
+#
+# add_address_to_hotels(hotels)
+# print(hotels)
