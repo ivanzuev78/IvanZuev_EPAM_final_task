19354c8f50841f48c601d413f95c2bf9cf635cbb
Ivan <ivan78.ru@yandex.ru>
2021-01-03 17:57:47 +0300

    threads added

--- geo_funcs/geo_funcs.py
+++ geo_funcs/geo_funcs.py
@@ -1,4 +1,5 @@
-from typing import Dict, List, Optional, Tuple
+from concurrent.futures.thread import ThreadPoolExecutor
+from typing import Dict, List, Tuple
 
 from geopy import Nominatim
 
@@ -28,22 +29,31 @@ def get_city_center(hotels: List[Dict]) -> Tuple[float, float]:
     return (min_latitude + max_latitude) / 2, (min_longitude + max_longitude) / 2
 
 
-def get_address_by_position(latitude: float, longitude: float) -> Optional[str]:
+def get_address_by_position(latitude: float, longitude: float) -> str:
     location = Nominatim(user_agent="ivan78").reverse(f"{latitude}, {longitude}")
     if location is not None:
         return location.address.replace(",", ";")
-    return None
+    return "The address is not found!"
 
 
-def add_address_to_hotels(hotels: List[Dict]) -> List[Dict]:
-    for hotel in hotels:
-        hotel["Address"] = get_address_by_position(
-            hotel["Latitude"], hotel["Longitude"]
-        )
-    return hotels
+def add_address_to_one_hotel(hotel: Dict) -> None:
+    hotel["Address"] = get_address_by_position(hotel["Latitude"], hotel["Longitude"])
 
 
-# column_names = {"Id": 0, "Name": 1, "Country": 2, "City": 3, "Latitude": 4, "Longitude": 5}
+def add_address_to_all_hotels(hotels: List[Dict], max_threads=100) -> None:
+    with ThreadPoolExecutor(max_workers=max_threads) as pool:
+        pool.map(add_address_to_one_hotel, hotels)
+
+
+#
+# column_names = {
+#     "Id": 0,
+#     "Name": 1,
+#     "Country": 2,
+#     "City": 3,
+#     "Latitude": 4,
+#     "Longitude": 5,
+# }
 # hotels_list = [
 #     ["1", "min_latitude", "US", "City", 0, 12.2345],
 #     ["2", "max_latitude", "US", "City", 90, 42],
@@ -53,6 +63,16 @@ def add_address_to_hotels(hotels: List[Dict]) -> List[Dict]:
 #     ["6", "Name", "US", "City", 33, 59.001],
 #     ["7", "Name", "US", "City", 89, 12],
 # ]
-# hotels = [{col: col_value for col, col_value in zip(column_names, hotel)} for hotel in hotels_list]
-# add_address_to_hotels(hotels)
-# print(*hotels, sep='\n')
+# hotels = [
+#     {col: col_value for col, col_value in zip(column_names, hotel)}
+#     for hotel in hotels_list
+# ]
+# t1 = time.time()
+# add_address_to_all_hotels(hotels, max_threads=2)
+#
+# # for i in hotels:
+# #     add_address_to_one_hotel(i)
+# t2 = time.time()
+#
+# print(t2 - t1)
+# print(*hotels, sep="\n")
