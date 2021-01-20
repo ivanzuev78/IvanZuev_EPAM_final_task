fd0619e64d44dbf487cb41c8a42cab9a2d32cd78
Ivan <ivan78.ru@yandex.ru>
2021-01-20 21:35:44 +0300

    save and load data funcs added

--- Data/save_data_funcs.py
+++ Data/save_data_funcs.py
@@ -1,18 +1,25 @@
 import json
-from typing import Dict
+from typing import Dict, List
 
 
-def save_all_hotels(all_hotels: Dict, all_city_centers: Dict) -> None:
-    with open("all_hotels.json", "w") as file:
-        json.dump(all_hotels, file)
-    with open("all_city_centers.json", "w") as file:
-        json.dump(all_city_centers, file)
+def save_all_hotels(
+    all_hotels: Dict, all_city_centers: Dict, biggest_cities: List
+) -> None:
+    all_hotels_data = {
+        "all_hotels": all_hotels,
+        "all_city_centers": all_city_centers,
+        "biggest_cities": biggest_cities,
+    }
+    with open("all_hotels_data.json", "w") as file:
+        json.dump(all_hotels_data, file)
 
 
 def load_all_hotels():
-    with open("all_hotels.json", "r") as file:
-        all_hotels = json.load(file)
-    with open("all_city_centers.json", "r") as file:
-        all_city_centers = json.load(file)
+    with open("all_hotels_data.json", "r") as file:
+        all_hotels_data = json.load(file)
 
-    return all_hotels, all_city_centers
+    return (
+        all_hotels_data["all_hotels"],
+        all_hotels_data["all_city_centers"],
+        all_hotels_data["biggest_cities"],
+    )
