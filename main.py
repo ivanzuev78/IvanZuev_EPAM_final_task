93311a4ebfe489057c3e2d5900dd5365862f4231
Ivan <ivan78.ru@yandex.ru>
2021-01-20 22:32:00 +0300

    main changed

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from Data.sort_hotels_data import (
 from Weather.graph_funcs import create_graph_with_min_and_max_temp
 from Weather.weather_forecast import get_all_weather
 
-from geo_funcs.geo_funcs import add_address_to_all_hotels, get_biggest_cities_centers
+from geo_funcs.geo_funcs import add_address_to_all_hotels, get_all_city_centers
 
 if __name__ == "__main__":
 
@@ -33,23 +33,20 @@ if __name__ == "__main__":
 
     # Получаем самые большие города
     biggest_cities = choose_biggest_cities(all_hotels)
-    print(biggest_cities)
+
     # Получаем все центры больших городов
-    biggest_centers = get_biggest_cities_centers(all_hotels, biggest_cities)
-    print(len(biggest_centers))
+    all_city_centers = get_all_city_centers(all_hotels, biggest_cities)
 
     # Мокаем add_address_to_all_hotels
     add_address_to_all_hotels = MagicMock(return_value=None)
     # проходимся по всем большим городам
     for county, city in biggest_cities:
-        print("get address:", county, city)
         # Доавляем адресс к каждому отелю в большом городе
         add_address_to_all_hotels(all_hotels[county][city])
-        print("get weather:", county, city)
         # Обогощаем отели в больших городах погодой
-        biggest_centers[county][city]["Weather"] = get_all_weather(
-            biggest_centers[county][city]["Latitude"],
-            biggest_centers[county][city]["Longitude"],
+        all_city_centers[county][city]["Weather"] = get_all_weather(
+            all_city_centers[county][city]["Latitude"],
+            all_city_centers[county][city]["Longitude"],
         )
 
     # Переходим в директорию для сохранения
@@ -57,5 +54,5 @@ if __name__ == "__main__":
     # Рисуем графики по наибольшим городам
     for county, city in biggest_cities:
         create_graph_with_min_and_max_temp(
-            biggest_centers[county][city]["Weather"], county, city
+            all_city_centers[county][city]["Weather"], county, city
         )
