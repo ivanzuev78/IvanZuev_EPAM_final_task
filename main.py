ab82a0830f01679110478a8a8ecab9e3d3756d0b
Ivan <ivan78.ru@yandex.ru>
2021-02-03 19:09:04 +0300

    It is old project now

--- main.py
+++ main.py
@@ -25,18 +25,18 @@ from Weather.weather_forecast import get_all_weather
 from geo_funcs.geo_funcs import add_address_to_all_hotels, get_biggest_city_centers
 
 if __name__ == "__main__":
-
+    print("Program started")
     parser = argparse.ArgumentParser(description="Hotels address and weather handler")
     parser.add_argument("indir", type=str, help="Input dir")
     parser.add_argument("outdir", type=str, help="Output dir")
-    parser.add_argument("--threads", type=int, help="Number of threads. Default: 4")
+    parser.add_argument("--threads", type=int, help="Number of threads. Default: 100")
     parser.add_argument("--appid", type=str, help="Appid to get weather")
     args = parser.parse_args()
 
     if args.threads:
         numb_of_threads = args.threads
     else:
-        numb_of_threads = 4
+        numb_of_threads = 100
 
     # Переходим в директорию с отелями
     os.chdir(args.indir)
@@ -45,21 +45,22 @@ if __name__ == "__main__":
         from Weather.appid import add_appid
 
         add_appid(args.appid)
-
+    print("read_csv_from_zip")
     # Считываем и сортируем отели
     all_hotels = sort_hotels_by_countries_and_cities(read_csv_from_zip())
-
+    print("choose_biggest_cities")
     # Получаем самые большие города
     biggest_cities = choose_biggest_cities(all_hotels)
-
+    print("get_biggest_city_centers")
     # Получаем все центры больших городов
     biggest_city_centers = get_biggest_city_centers(all_hotels, biggest_cities)
-
     # проходимся по всем большим городам
     for county, city in biggest_cities:
+        print("add_address_to_all_hotels", county, city)
         # Доавляем адресс к каждому отелю в большом городе
         add_address_to_all_hotels(all_hotels[county][city], max_threads=numb_of_threads)
         # Обогощаем большие города погодой
+        print("get_all_weather", county, city)
         biggest_city_centers[county][city]["Weather"] = get_all_weather(
             biggest_city_centers[county][city]["Latitude"],
             biggest_city_centers[county][city]["Longitude"],
@@ -68,7 +69,7 @@ if __name__ == "__main__":
 
     # Переходим в директорию для сохранения
     os.chdir(args.outdir)
-
+    print("post_processing")
     # Рисуем графики по наибольшим городам
     for county, city in biggest_cities:
         create_graph_with_min_and_max_temp(
@@ -98,3 +99,5 @@ if __name__ == "__main__":
 
     with open("post_process_data.json", "w") as file:
         json.dump(post_process_data, file, indent=4)
+
+    print("Program finished")
